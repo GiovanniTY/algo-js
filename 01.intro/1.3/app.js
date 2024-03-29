28a65b4b3e12c0702cc72cb01d0db19ccf40737f
Giovanni_T <giovannitummino.15@gmail.com>
2024-03-29 11:24:51 +0100

    Excercis1.3 fini

---
+++ 01.intro/1.3/app.js
@@ -0,0 +1,7 @@
+(() => {
+   let name = "Tummino";
+   let first_name = 'Giovanni';
+   let city = 'Chapelle';
+
+   console.log('My name is' + ' ' + first_name + ' ' + name + ' ' + 'I live'+ ' ' + 'in' + ' ' + city)
+})();
