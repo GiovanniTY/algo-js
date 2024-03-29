d9eaefa4bb81b3f103e1c225262a185a33662dd3
Giovanni_T <giovannitummino.15@gmail.com>
2024-03-29 19:12:14 +0100

    Excercise 3.0 fini

---
+++ 03.intro/3.0/app.js
@@ -0,0 +1,35 @@
+(() => {
+    const studentsOfBecode = [
+        { nom: 'Lyne' },
+        { nom: 'Dj3y' },
+        { nom: 'Zara' },
+        { nom: 'Manu' },
+        { nom: 'Adrien' },
+        { nom: 'NathAmd' },
+        { nom: 'Xeless' },
+        { nom: 'Caroline' },
+        { nom: 'Arnaud' },
+        { nom: 'Stacy' },
+        { nom: 'Justine' },
+        { nom: 'Angel' },
+        { nom: 'Dylan' },
+        { nom: 'Adca' },
+        { nom: 'isabelle' },
+        { nom: 'doblaria' },
+        { nom: 'Youri' },
+        { nom: 'Giovanni' },
+        { nom: 'Iliess' },
+        { nom: 'Justine L' },
+        { nom: 'Denis' },
+        { nom: 'Mohamed' },
+        { nom: 'ludovic' },
+        { nom: 'Maryam' },
+        { nom: 'Julie' },
+        { nom: 'Jordan' },
+        { nom: 'Tasha' },
+    ];
+
+    for (const student of studentsOfBecode) {
+        console.log(student.nom);
+    }
+})();
