ea6f5087d1e2de349af527a67c099963842db634
Giovanni_T <giovannitummino.15@gmail.com>
2024-03-29 10:39:41 +0100

    Mod Javascript

--- 01.intro/1.1/app.js
+++ 01.intro/1.1/app.js
@@ -1,2 +1,20 @@
-const num = 2 + 2;
-console.log(num);
+// Addition
+const add = 10 + 10;
+// Print
+console.log(add);
+// Soustraction 
+const sous = 10 - 5;
+// Print 
+console.log(sous); 
+// Multiplication
+const mult = 19 * 10;
+// Print 
+console.log(mult);
+// Division
+const div = 10 / 2;
+// Print 
+console.log(div);
+// Modulo
+const mod = 10 % 10;
+// Print
+console.log(mod);
