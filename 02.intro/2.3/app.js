193beb0da5d2e945fb94b0c14bef3dcf9a3e2723
Giovanni_T <giovannitummino.15@gmail.com>
2024-03-29 13:58:57 +0100

    Excercise 2.3 fini

---
+++ 02.intro/2.3/app.js
@@ -0,0 +1,18 @@
+(() => {
+// Definition variable
+let i = 2;
+while (i <= 100) {
+    console.log(i);
+    i += 2;
+}
+// Bonus with while
+let a = 2;
+while (a <= 200) {
+    console.log(a);
+    a += 2;
+}
+// Bonus with for 
+for (let b = 2; b <= 100; b += 2) {
+    console.log(b);
+  }
+})();
